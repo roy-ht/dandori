e4e8b5d01f9ebb1bbc9a8c02a7ce3d8853e514b6
Hiroyuki Tanaka <aflc0x@gmail.com>
2021-06-29 11:51:14 +0900

    🔥  samples

--- sample/handler/__init__.py
+++
@@ -1,5 +0,0 @@
-def handle_issue_comment(ctx):
-    """handle issue_comment event, some slash command"""
-    body = ctx.gh.comment_body()
-    if body.startswith("/greet"):
-        print("Local greet command invoked!!!")
